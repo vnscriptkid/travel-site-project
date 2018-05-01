1e4ffe645b8ec991df72fe2b6ccc70b87805bf70
vnscriptkid <vnscriptkid@gmail.com>
2018-05-01 23:20:04 +0700

    hero and beginning sections done

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,6 @@
-const gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer'),
-	cssvars = require('postcss-simple-vars'),
-	nested = require('postcss-nested');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', () => {
-	console.log('This is a default gulp task');
-});
 
-gulp.task('html', () => {
-	console.log('Something useful with html');
-});
 
-gulp.task('styles', () => {
-	return gulp
-		.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', () => {
-	watch('./app/index.html', () => {
-		gulp.start('html');
-	});
-
-	watch('./app/assets/styles/**/*.css', () => {
-		gulp.start('styles');
-	});
-});
