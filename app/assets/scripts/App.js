b7d865d2077ca35554d45100e2edf7059ffb6052
vnscriptkid <vnscriptkid@gmail.com>
2018-05-03 16:16:08 +0700

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 let mobileMenu = new MobileMenu();
 let revealOnScrollToFeatures = new RevealOnScroll($('.feature-item'), '85%');
 let revealOnScrollToTestimonials = new RevealOnScroll($('.testimonial'), '60%');
 let stickyHeader = new StickyHeader();
+let modal = new Modal();
